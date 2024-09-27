8565d743c03637e4ba6e0d86d193b40e4d7f7ae7
Michelle <109476419+ChellySil@users.noreply.github.com>
2024-09-27 14:52:27 -0300

    pequena

--- infra.distribuida/cadastro/cadastro/src/main/java/com/trabalho/cadastro/service/CadastroService.java
+++ infra.distribuida/cadastro/cadastro/src/main/java/com/trabalho/cadastro/service/CadastroService.java
@@ -1,20 +1,17 @@
 package com.trabalho.cadastro.service;
 
-import com.exemplo.cadastro.model.Usuario;
+import com.trabalho.cadastro.model.Usuario;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CadastroService {
-
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     public void cadastrarUsuario(Usuario usuario) {
-      
         System.out.println("Usuário cadastrado: " + usuario.getNome());
-
         rabbitTemplate.convertAndSend("notificacao.queue", usuario);
     }
 }
