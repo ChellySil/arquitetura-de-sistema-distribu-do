56215bf3db192b928d294bc5de6b3d6c42e6d5b6
Michelle <109476419+ChellySil@users.noreply.github.com>
2024-09-27 14:49:52 -0300

    Add files via upload

---
+++ infra.distribuida/cadastro/cadastro/src/main/java/com/trabalho/cadastro/service/CadastroService.java
@@ -0,0 +1,20 @@
+package com.trabalho.cadastro.service;
+
+import com.exemplo.cadastro.model.Usuario;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroService {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    public void cadastrarUsuario(Usuario usuario) {
+      
+        System.out.println("Usuário cadastrado: " + usuario.getNome());
+
+        rabbitTemplate.convertAndSend("notificacao.queue", usuario);
+    }
+}
