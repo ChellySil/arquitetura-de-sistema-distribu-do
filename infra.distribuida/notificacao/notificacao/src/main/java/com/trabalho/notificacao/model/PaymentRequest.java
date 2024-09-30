5df2375656724c2f534cb090fe959a255d7479cc
Michelle <109476419+ChellySil@users.noreply.github.com>
2024-09-30 11:37:51 -0300

    pagamento

---
+++ infra.distribuida/notificacao/notificacao/src/main/java/com/trabalho/notificacao/model/PaymentRequest.java
@@ -0,0 +1,22 @@
+package com.trabalho.notificacao.model;
+
+public class PaymentRequest {
+    private double valor;
+    private String descricao;
+
+    
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
